2b4c397f71cc1c9a89f35d6ed9bc7a30625d40e4
MuhammadAman02 <mohammada@hst.ie>
2025-05-12 14:27:50 +0500

    update file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "./index.css";
+import { ThemeProvider } from "./components/theme/ThemeProvider";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="cu-shield-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
